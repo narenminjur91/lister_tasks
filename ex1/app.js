6e52ed4d5e23262563f4ef56c28a8cee2f8d8bee
Narendra Minjur <narendra.m@lt-mac-04.lister.lan>
2019-04-23 17:02:54 +0530

    Templating using Nunjuks
    Included Nunjuks with other required resource and the template JSON for the index page.
    Modified index page to render it using Nunjuks templating

--- ex1/app.js
+++ ex1/app.js
@@ -1,17 +1,37 @@
+//Require modules
 var express = require('express');
-var app = express();
-var port = 8000;
+var fs = require('fs');
+var nunjucks = require("nunjucks");
 
-app.get('/', function(req, res){
-  res.sendFile('index.html',{root:__dirname});
-});
-app.get('/uikit-3.0.3/:dirname/:filename', function(req, res) {
-  res.sendFile('uikit-3.0.3/'+req.params.dirname+'/'+req.params.filename,{root:__dirname});
-});
-app.get('/img/:filename', function(req, res) {
-  res.sendFile('img/'+req.params.filename,{root:__dirname});
+var app = express();//Object to use the Express API
+var port = 8000;//Port number to listen
+
+//Configure Nunjuks with the required options
+nunjucks.configure(__dirname, {
+  autoescape: true,
+  express: app
 });
 
+//Listen using the specified port
 app.listen(port, function(){
-  console.log("Litening on port:"+port);
+  console.log("Listening on port:"+port);
+});
+
+//Redirect locahost to index.html
+app.get('/', function(req, res){
+  app.use(express.static(__dirname));//Add static middleware to serve css, js and img.
+
+  //Read JSON file to get the contents for template
+  fs.readFile("template_data.json", function(err, data){
+    if(!err)
+    {
+      data = JSON.parse(data);
+      res.render('index.html', data);//Render the template using the JSON data from the JSON file.
+    }
+    else
+    {
+      res.render('error_500.html');//Render error page if JSON file could not be read.
+      console.error(err);
+    }
+  });
 });
